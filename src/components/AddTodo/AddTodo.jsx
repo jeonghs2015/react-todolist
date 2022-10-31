8ee0d6d4c2f095a4b3b3c037fa5b3b2300084b9c
hsjung2015 <hsjung201558@gmail.com>
2022-10-31 14:07:17 +0900

    uuid(unique id 값 생성) 추가

--- src/components/AddTodo/AddTodo.jsx
+++ src/components/AddTodo/AddTodo.jsx
@@ -1,11 +1,15 @@
 import React, { useState } from 'react'
+import { v4 as uuidv4 } from 'uuid';
 
 export default function AddTodo( { onAdd } ) {
     const [text, setText] = useState('');
     const handleChange = (e) => setText(e.target.value);
     const handleSubmit = (e) => {
       e.preventDefault();
-      onAdd({id: '', text, status: 'active'});
+      if(text.trim().length === 0){
+        return;
+      }
+      onAdd({id: uuidv4(), text, status: 'active'});
       setText('');
     }
   return(
