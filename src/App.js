1ce5e723fe0ccb8ca80f64287460113f033d7811
hsjung2015 <hsjung201558@gmail.com>
2022-10-31 16:19:27 +0900

    App에 filter 관련 내용 추가

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import { useState } from 'react';
 import './App.css';
 import TodoList from './components/TodoList/TodoList';
+import Header from './components/Header/Header';
 
+const filters = ['all', 'active', 'completed'];
 function App() {
+  const [filter, setFilter] = useState(filters[0]);
   return (
     <>
-      <TodoList />
+      <Header filters={filters} filter={filter} onFilterChange={setFilter} />
+      <TodoList filter={filter} />
     </>
   );
 }
