77c4c4e6ef0fcd689938b9187bc78dd56c0ec313
hsjung2015 <hsjung201558@gmail.com>
2022-10-31 17:08:55 +0900

    style 설정 작업1

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
+import styles from './Header.module.css';
 
 export default function Header({filters, filter, onFilterChange}) {
   return (
-    <div>
-        <ul>
+    <header className={styles.header}>
+        <ul className={styles.filters}>
             {filters.map((value, index) => 
-                <li key={index}>
-                    <button onClick={() => onFilterChange(value)}>{value}</button>
+                <li key={index} >
+                    <button className={`${styles.filter} ${filter === value && styles.selected}`} onClick={() => onFilterChange(value)}>{value}</button>
                 </li>)}
         </ul>
-    </div>
+    </header>
   )
 }
