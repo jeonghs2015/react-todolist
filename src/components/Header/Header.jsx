5f124c502be12e7dab566472602eaeb9bfdfa40e
hsjung2015 <hsjung201558@gmail.com>
2022-10-31 16:21:40 +0900

    filter 값 변경 코드 추가

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
 
-export default function Header() {
+export default function Header({filters, filter, onFilterChange}) {
   return (
-    <div>Header</div>
+    <div>
+        <ul>
+            {filters.map((value, index) => 
+                <li key={index}>
+                    <button onClick={() => onFilterChange(value)}>{value}</button>
+                </li>)}
+        </ul>
+    </div>
   )
 }
