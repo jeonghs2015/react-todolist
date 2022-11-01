ace7f24d7b537092f418025e7084e8d270799214
hsjung2015 <hsjung201558@gmail.com>
2022-11-01 15:29:20 +0900

    dark 모드 작업 마무리

--- src/context/DarkModeContext.jsx
+++ src/context/DarkModeContext.jsx
@@ -1,12 +1,22 @@
-import { createContext, useContext, useState } from 'react';
+import { createContext, useContext, useEffect, useState } from 'react';
 
 const DarkModeContext = createContext();
 
 export function DarkModeProvider({children}){
     const [darkMode, setDarkMode] = useState(false);
     const toggleDarkMode = () => {
-        setDarkMode((prev) => !prev);
+        setDarkMode(!darkMode);
+        updateDarkMode(!darkMode);
     };
+
+    useEffect(() => {
+        const isDark = localStorage.theme === 'dark' ||
+        (!('theme' in localStorage) &&
+        window.matchMedia('(prefers-color-scheme: dark)').matches);
+        setDarkMode(isDark);
+        updateDarkMode(isDark);
+    }, []);
+    
     return(
         <DarkModeContext.Provider value={{ darkMode, toggleDarkMode }}>
             {children}
@@ -14,4 +24,11 @@ export function DarkModeProvider({children}){
     );
 }
 
+function updateDarkMode(darkMode) {
+    if(darkMode) {
+        document.documentElement.classList.add('dark');
+    } else {
+        document.documentElement.classList.remove('dark');
+    }
+}
 export const useDarkMode = () => useContext(DarkModeContext);
